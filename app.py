a200d692b20e460deef35b67cf07bc4b0dfbcfff
Wenastardes <94759147+Wenastardes@users.noreply.github.com>
2026-02-18 19:51:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -367,7 +367,7 @@ def cambiar_contrasena():
 # INICIAR APLICACIÓN
 # ============================================================================
 
-import os
+if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-#Forzar rebuild#
